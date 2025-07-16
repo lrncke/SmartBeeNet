a560f62780db838bd30f4612e10a5e94ac5e62b7
lrncke <132354532+lrncke@users.noreply.github.com>
2025-07-16 17:08:41 +0200

    Add files via upload

--- Code/HX711_Test.ino
+++ Code/HX711_Test.ino
@@ -1,92 +1,111 @@
-/*
-   -------------------------------------------------------------------------------------
-   HX711_ADC
-   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
-   Olav Kallhovd sept2017
-   -------------------------------------------------------------------------------------
-*/
-
-/*
-   Settling time (number of samples) and data filtering can be adjusted in the config.h file
-   For calibration and storing the calibration value in eeprom, see example file "Calibration.ino"
-
-   The update() function checks for new data and starts the next conversion. In order to acheive maximum effective
-   sample rate, update() should be called at least as often as the HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS.
-   If you have other time consuming code running (i.e. a graphical LCD), consider calling update() from an interrupt routine,
-   see example file "Read_1x_load_cell_interrupt_driven.ino".
-
-   This is an example sketch on how to use this library
-*/
-
 #include <HX711_ADC.h>
-#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#if defined(ESP8266) || defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
 
-//pins:
-const int HX711_dout = 22; //mcu > HX711 dout pin
-const int HX711_sck = 23; //mcu > HX711 sck pin
+// Pins für jeden HX711
+HX711_ADC LoadCell1(22, 23);
+HX711_ADC LoadCell2(18, 19);
+HX711_ADC LoadCell3(4, 5);
+HX711_ADC LoadCell4(16, 17);
 
-//HX711 constructor:
-HX711_ADC LoadCell(HX711_dout, HX711_sck);
+// Kalibrierungswerte (je HX711 individuell)
+float calibrationValue1 = 224.92;
+float calibrationValue2 = 225.10;
+float calibrationValue3 = 223.80;
+float calibrationValue4 = 224.50;
 
-const int calVal_eepromAdress = 0;
+const float offsetWeight = 2.5; // kg oder Gramm, je nach Kalibrierung
+
 unsigned long t = 0;
 
 void setup() {
-  Serial.begin(57600); delay(10);
+  Serial.begin(57600);
+  delay(10);
   Serial.println();
   Serial.println("Starting...");
 
-  LoadCell.begin();
-  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-  float calibrationValue; // calibration value (see example file "Calibration.ino")
-  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
-#if defined(ESP8266)|| defined(ESP32)
-  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
-#endif
-  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+  // Start aller HX711
+  LoadCell1.begin();
+  LoadCell2.begin();
+  LoadCell3.begin();
+  LoadCell4.begin();
 
-  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
-  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
-  LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag()) {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+  // Kalibrierung setzen
+  LoadCell1.setCalFactor(calibrationValue1);
+  LoadCell2.setCalFactor(calibrationValue2);
+  LoadCell3.setCalFactor(calibrationValue3);
+  LoadCell4.setCalFactor(calibrationValue4);
+
+  unsigned long stabilizingtime = 2000;
+  boolean _tare = true;
+
+  LoadCell1.start(stabilizingtime, _tare);
+  LoadCell2.start(stabilizingtime, _tare);
+  LoadCell3.start(stabilizingtime, _tare);
+  LoadCell4.start(stabilizingtime, _tare);
+
+  if (LoadCell1.getTareTimeoutFlag() || LoadCell2.getTareTimeoutFlag() ||
+      LoadCell3.getTareTimeoutFlag() || LoadCell4.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check wiring!");
     while (1);
   }
-  else {
-    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
-    Serial.println("Startup is complete");
-  }
+  Serial.println("Startup complete.");
 }
 
 void loop() {
   static boolean newDataReady = 0;
-  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+  const int serialPrintInterval = 500; // Ausgabe-Intervall in ms
 
-  // check for new data/start next conversion:
-  if (LoadCell.update()) newDataReady = true;
+  // Update aller Sensoren
+  bool newData = false;
+  newData |= LoadCell1.update();
+  newData |= LoadCell2.update();
+  newData |= LoadCell3.update();
+  newData |= LoadCell4.update();
+  if (newData) newDataReady = true;
 
-  // get smoothed value from the dataset:
+  // Gewichte auslesen, addieren, Offset abziehen
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
-      float i = LoadCell.getData();
-      Serial.print("Load_cell output val: ");
-      Serial.println(i);
+      float weight1 = LoadCell1.getData();
+      float weight2 = LoadCell2.getData();
+      float weight3 = LoadCell3.getData();
+      float weight4 = LoadCell4.getData();
+
+      float totalWeight = weight1 + weight2 + weight3 + weight4 - offsetWeight;
+
+      Serial.print("HX711 #1: ");
+      Serial.print(weight1);
+      Serial.print("\t HX711 #2: ");
+      Serial.print(weight2);
+      Serial.print("\t HX711 #3: ");
+      Serial.print(weight3);
+      Serial.print("\t HX711 #4: ");
+      Serial.print(weight4);
+      Serial.print("\t => Total (with offset -");
+      Serial.print(offsetWeight);
+      Serial.print("): ");
+      Serial.println(totalWeight);
+
       newDataReady = 0;
       t = millis();
     }
   }
 
-  // receive command from serial terminal, send 't' to initiate tare operation:
+  // Tare per Terminalbefehl 't'
   if (Serial.available() > 0) {
     char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay();
+    if (inByte == 't') {
+      LoadCell1.tareNoDelay();
+      LoadCell2.tareNoDelay();
+      LoadCell3.tareNoDelay();
+      LoadCell4.tareNoDelay();
+    }
   }
 
-  // check if last tare operation is complete:
-  if (LoadCell.getTareStatus() == true) {
-    Serial.println("Tare complete");
+  if (LoadCell1.getTareStatus() || LoadCell2.getTareStatus() ||
+      LoadCell3.getTareStatus() || LoadCell4.getTareStatus()) {
+    Serial.println("Tare complete.");
   }
-
 }
